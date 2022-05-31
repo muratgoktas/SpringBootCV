b038c33c0c769338d16a0931e28f10c605c04848
Murat Goktas <mcs.goktas@gmail.com>
2022-05-31 11:51:18 +0300

    Mg-03) Created springboot temp.

---
+++ src/main/java/com/cvpro/springbootcv/SpringbootcvApplication.java
@@ -0,0 +1,13 @@
+package com.cvpro.springbootcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcvApplication.class, args);
+	}
+
+}
